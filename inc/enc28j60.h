28c189fae6998593c21bc55fc607a02473e3c7d9
redhat2410 <ducvuminhspk@gmail.com>
2019-03-29 22:09:49 +0700

    Upload by ducvu

--- inc/enc28j60.h
+++ inc/enc28j60.h
@@ -1,6 +1,6 @@
 /*****************************Description ENC28J60****************************/
 /*
-
+		
 */
 /*****************************Description ENC28J60****************************/
 #define __ENC28J60_H__
@@ -111,5 +111,95 @@
 #define	ECOCON		(0x15	|	0x60	|	0x80)
 #define	EFLOCON		(0x17	|	0x60	|	0x80)
 #define	EPAUS			(0x18	|	0x60	|	0x80)
+/*************ERXFCON register bit definitions**************/
+#define ERXFCON_UCEN     0x80
+#define ERXFCON_ANDOR    0x40
+#define ERXFCON_CRCEN    0x20
+#define ERXFCON_PMEN     0x10
+#define ERXFCON_MPEN     0x08
+#define ERXFCON_HTEN     0x04
+#define ERXFCON_MCEN     0x02
+#define ERXFCON_BCEN     0x01
+/*************EIE Register Bit Definitions**************/
+#define EIE_INTIE        0x80
+#define EIE_PKTIE        0x40
+#define EIE_DMAIE        0x20
+#define EIE_LINKIE       0x10
+#define EIE_TXIE         0x08
+#define EIE_WOLIE        0x04
+#define EIE_TXERIE       0x02
+#define EIE_RXERIE       0x01
+/*************EIR Register Bit Definitions**************/
+#define EIR_PKTIF        0x40
+#define EIR_DMAIF        0x20
+#define EIR_LINKIF       0x10
+#define EIR_TXIF         0x08
+#define EIR_WOLIF        0x04
+#define EIR_TXERIF       0x02
+#define EIR_RXERIF       0x01
+/*************ESTAT Register Bit Definitions**************/
+#define ESTAT_INT        0x80
+#define ESTAT_LATECOL    0x10
+#define ESTAT_RXBUSY     0x04
+#define ESTAT_TXABRT     0x02
+#define ESTAT_CLKRDY     0x01
+/*************ECON2 Register Bit Definitions**************/
+#define ECON2_AUTOINC    0x80
+#define ECON2_PKTDEC     0x40
+#define ECON2_PWRSV      0x20
+#define ECON2_VRPS       0x08
+/*************ECON1 Register Bit Definitions**************/
+#define ECON1_TXRST      0x80
+#define ECON1_RXRST      0x40
+#define ECON1_DMAST      0x20
+#define ECON1_CSUMEN     0x10
+#define ECON1_TXRTS      0x08
+#define ECON1_RXEN       0x04
+#define ECON1_BSEL1      0x02
+#define ECON1_BSEL0      0x01
+/*************MACON1 Register Bit Definitions**************/
+#define MACON1_LOOPBK    0x10
+#define MACON1_TXPAUS    0x08
+#define MACON1_RXPAUS    0x04
+#define MACON1_PASSALL   0x02
+#define MACON1_MARXEN    0x01
+/*************MACON3 Register Bit Definitions**************/
+#define MACON3_PADCFG2   0x80
+#define MACON3_PADCFG1   0x40
+#define MACON3_PADCFG0   0x20
+#define MACON3_TXCRCEN   0x10
+#define MACON3_PHDRLEN   0x08
+#define MACON3_HFRMLEN   0x04
+#define MACON3_FRMLNEN   0x02
+#define MACON3_FULDPX    0x01
+/*************MICMD Register Bit Definitions**************/
+#define MICMD_MIISCAN    0x02
+#define MICMD_MIIRD      0x01
+/*************MISTAT Register Bit Definitions**************/
+#define MISTAT_NVALID    0x04
+#define MISTAT_SCAN      0x02
+#define MISTAT_BUSY      0x01
+/*************EBSTCON Register Bit Definitions**************/
+#define EBSTCON_PSV2     0x80
+#define EBSTCON_PSV1     0x40
+#define EBSTCON_PSV0     0x20
+#define EBSTCON_PSEL     0x10
+#define EBSTCON_TMSEL1   0x08
+#define EBSTCON_TMSEL0   0x04
+#define EBSTCON_TME      0x02
+#define EBSTCON_BISTST    0x01
+
+/**********************PHY registers***********************/
+#define PHCON1           0x00
+#define PHSTAT1          0x01
+#define PHHID1           0x02
+#define PHHID2           0x03
+#define PHCON2           0x10
+#define PHSTAT2          0x11
+#define PHIE             0x12
+#define PHIR             0x13
+#define PHLCON           0x14
+
+
 /*********************Memory Oganization*********************/
 
