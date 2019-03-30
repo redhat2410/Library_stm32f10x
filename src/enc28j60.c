c06db74da18465474bc59f81d1790f11ede28e37
redhat2410 <ducvuminhspk@gmail.com>
2019-03-30 14:35:51 +0700

    Upload by ducvu

--- src/enc28j60.c
+++ src/enc28j60.c
@@ -10,8 +10,56 @@
 #define		NSS		8
 
 void initSPI(void){
+	//Config pinout for SPI
+	//Config pin SCK
+	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_AFPUSHPULL, SCK);
+	//Config pin MISO
+	GPIO_configuration(GPIOPORT_A, MODE_INPUT, IN_FLOATING_MODE, MISO);
+	//Config pin MOSI
+	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_AFPUSHPULL, MOSI);
+	//Config pin NSS
+	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_PUSHPULL, NSS);
 	//Conifguration SPI in mode MASTER, data bit 8bit, div SCK 16, interrupt NO
 	SPI_configuration(SPIx1, SPI_MASTER, SPI_DFF_8_BITS, SPI_FCLK_16, SPI_NO, 0);
-	//Config pinout for SPI
-	
 }
+
+void enableChip(void){
+	//Set pin NSS to LOW (0)
+	GPIO_WriteBit(GPIOPORT_A, NSS, BIT_RESET);
+}
+
+void disableChip(void){
+	//Set pin NSS to HIGH (1)
+	GPIO_WriteBit(GPIOPORT_A, NSS, BIT_SET);
+}
+
+void writeOp(uint8_t op, uint8_t address, uint8_t data){
+	//enable chip
+	enableChip();
+	//send operation and address of register to enc28j60
+	SPI_WriteData(SPIx1, op | address);
+	//send data will be written for register
+	SPI_WriteData(SPIx1, data);
+	//disable chip
+	disableChip();
+}
+
+uint8_t readOp(uint8_t op, uint8_t address){
+	uint8_t rev = 0;
+	//enable chip
+	enableChip();
+	//Send operation and address of register to enc28j60
+	SPI_WriteData(SPIx1, op | address);
+	SPI_WriteData(SPIx1, 0x00);
+	//Verify address is MII or MAC
+	if(address & 0x80)
+		SPI_WriteData(SPIx1, 0x00);
+	//Receiver data from enc28j60
+	rev = SPI_ReadData(SPIx1);
+	//disable chip
+	disableChip();
+	return rev;
+}
+
+
+
--- src/main.c
+++ src/main.c
@@ -10,29 +10,16 @@
 #include "math.h"
 
 
-int dem= 0;
+uint8_t dem= 0;
 void delay(long time);
 int main(){
 	peripheral_register_init();
 	SystemInit();
-	//Cau hinh pin C13 output
-	GPIO_configuration(GPIOPORT_C, MODE_OUTPUT_50M, OUT_PUSHPULL, 13);
-	//Configuration pinout for SPI1
-	//NSS
-	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_AFPUSHPULL, 4);
-	//SCK
-	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_AFPUSHPULL, 5);
-	//MISO
-	GPIO_configuration(GPIOPORT_A, MODE_INPUT,	IN_FLOATING_MODE, 6);
-	//MOSI
-	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_AFPUSHPULL, 7);
+	initSPI();
+	writeOp(ENC28J60_BIT_FIELD_CLR, ECON1, ECON1_BSEL1|ECON1_BSEL0);
+	delay(10000);
+	dem = readOp(ENC28J60_READ_CTRL_REG, ECON1);
 	
-	SPI_configuration(SPIx1, SPI_MASTER, SPI_DFF_8_BITS, SPI_FCLK_8, SPI_NO, SPI1_IRQn); 
-	while(1){
-		SPI_WriteData(SPIx1, 0x01);
-		dem = SPI_ReadData(SPIx1);
-		
-	}
 }
 
 void delay(long time){
