176974a1bd54eb9596c2b31f51b960969a3a47cc
redhat2410 <ducvuminhspk@gmail.com>
2019-04-26 22:30:54 +0700

    Upload by ducvu

--- src/main.c
+++ src/main.c
@@ -10,6 +10,9 @@
 
 uint16_t dem= 0x0000;
 
+uint8_t temp_1 = 0x00;
+uint8_t temp_2 = 0x00;
+
 int main(){
 	SPI_Configure config;
 	
@@ -29,22 +32,33 @@ int main(){
 	GPIO_configuration(GPIOPORT_A, MODE_OUTPUT_50M, OUT_PUSHPULL, 4);
 	
 	SPI_configuration(SPIx1, SPI_MASTER, SPI_DFF_8_BITS, SPI_FCLK_128, SPI_NO, SPI1_IRQn);
-	/*
+	
 	config.spi 					= SPIx1;
 	config.spiMode 			= SPI_MASTER;
 	config.spiFrame 		= SPI_DFF_8_BITS;
 	config.spiBaudrate	= SPI_FCLK_128;
 	config.spiIT				=	SPI_NO;
-	config.spiFirst			=	SPI_FirstMSB;
+	config.spiFirst			=	SPI_FirstLSB;
 	config.spiModeClock	=	SPI_CLOCK_1;
 	config.spiNSS				=	SPI_NSS_Software;
-	SPI_Init(config);*/
+	SPI_Init(config);
 	
+	GPIO_WriteBit(GPIOPORT_C, 13, BIT_SET);
 	
 	while(1){
+		//Write data in control register
 		GPIO_WriteBit(GPIOPORT_C, 13, BIT_RESET);
-		SPI_WriteData(SPIx1, 0xAA);
-		dem = SPI_ReadData(SPIx1);
+		SPI_WriteData(SPIx1, ENC28J60_WRITE_CTRL_REG | ECON1 );
+		delay_ms(10);
+		SPI_WriteData(SPIx1, ECON1_BSEL0); // ECON1_BSEL1   ECON1_BSEL0
+		GPIO_WriteBit(GPIOPORT_C, 13, BIT_SET);
+		//delay_ms(50);
+		
+		//Read dat in control register
+		GPIO_WriteBit(GPIOPORT_C, 13, BIT_RESET);
+		SPI_WriteData(SPIx1, ENC28J60_READ_CTRL_REG | ECON1 );
+		delay_us(10);
+		temp_1 = SPI_ReadData(SPIx1);
 		GPIO_WriteBit(GPIOPORT_C, 13, BIT_SET);
 	}
 	
